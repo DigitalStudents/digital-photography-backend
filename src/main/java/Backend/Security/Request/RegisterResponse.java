51a783b4c5780be79f7931aec49438f812d8b6ef
martinezback <martinezhenry703@gmail.com>
2023-10-29 22:48:38 -0500

    register response

---
+++ src/main/java/Backend/Security/Request/RegisterResponse.java
@@ -0,0 +1,16 @@
+package Backend.Security.Request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponse {
+    private String token;
+    private String message;
+    private String email;
+}
