71fbe02dc6513872378db0f2406beacf277def78
martinezback <martinezhenry703@gmail.com>
2023-10-29 22:41:08 -0500

    user repository

---
+++ src/main/java/Backend/User/Repository/UserRepository.java
@@ -0,0 +1,14 @@
+package Backend.User.Repository;
+
+import Backend.User.Model.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+}
