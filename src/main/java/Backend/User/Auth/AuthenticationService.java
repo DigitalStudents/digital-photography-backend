be90e3b2f21edf9b23deeb1e64105de1280ce143
StonyKing <ramiro_hero@hotmail.com>
2023-11-25 15:02:22 -0300

    JWT Token now have user role & ID

--- src/main/java/Backend/User/Auth/AuthenticationService.java
+++ src/main/java/Backend/User/Auth/AuthenticationService.java
@@ -5,6 +5,7 @@ import Backend.User.Model.ERole;
 import Backend.User.Model.UserEntity;
 import Backend.User.dto.UserEntityDTO;
 import Backend.exceptions.BadRequestException;
+import com.amazonaws.services.kms.model.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,6 +13,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class AuthenticationService implements IAuthenticationService {
@@ -36,16 +39,21 @@ public class AuthenticationService implements IAuthenticationService {
 
     @Override
     public String login(AuthenticationRequest authenticationRequest) {
-
         try {
             UsernamePasswordAuthenticationToken login = new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword());
             Authentication authentication = authenticationManager.authenticate(login);
-            return jwtUtils.generateAccesToken(authenticationRequest.getUsername());
+
+            UserEntity user = getUser(authenticationRequest.getUsername());
+
+            return jwtUtils.generateAccessToken(authenticationRequest.getUsername(), user.getId(), user.getRole());
         } catch (AuthenticationException ex) {
             throw new BadRequestException("Error de autenticación");
         }
-
     }
 
+    private UserEntity getUser(String username) {
+        Optional<UserEntity> userEntityOptional = userRepository.findByUsername(username);
+        return userEntityOptional.orElseThrow(() -> new NotFoundException("Usuario no encontrado"));
+    }
 
 }
