f4cf625a45c0b5e5977572cf64b44c9608cab647
martinezback <martinezhenry703@gmail.com>
2023-12-05 11:21:48 -0500

    test

---
+++ src/test/java/Backend/Caracteristicas/CaracteristicaServiceImplTest.java
@@ -0,0 +1,28 @@
+package Backend.Caracteristicas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaracteristicaServiceImplTest {
+
+    @Test
+    void crearCaracteristica() {
+    }
+
+    @Test
+    void buscarCaracteristica() {
+    }
+
+    @Test
+    void traerTodos() {
+    }
+
+    @Test
+    void modificarCaracteristica() {
+    }
+
+    @Test
+    void eliminarCaracteristica() {
+    }
+}
