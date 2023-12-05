3b68331b79b3fc981eabb712ce2f709acdb11ed6
StonyKing <ramiro_hero@hotmail.com>
2023-12-05 02:49:12 -0300

    casi me olvido los permisos

--- src/main/java/Backend/Security/SecurityConfig.java
+++ src/main/java/Backend/Security/SecurityConfig.java
@@ -36,9 +36,8 @@ public class SecurityConfig {
                     auth.requestMatchers("/send-test-email").permitAll();
 
                     auth.requestMatchers(HttpMethod.GET,"/v1/**").permitAll();
-                    auth.requestMatchers(HttpMethod.PUT, "/v1/**").permitAll();
-                    auth.requestMatchers(HttpMethod.DELETE, "/v1/**").permitAll();
-                    auth.requestMatchers(HttpMethod.POST, "/v1/**").permitAll();
+                    auth.requestMatchers(HttpMethod.POST, "/v1/reservations").hasRole("USER");
+
 
                     auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
 
@@ -47,7 +46,9 @@ public class SecurityConfig {
                     auth.requestMatchers(HttpMethod.POST, "/user/**").hasRole("ADMIN");
                     auth.requestMatchers(HttpMethod.PUT, "/user/**").hasRole("ADMIN");
                     auth.requestMatchers(HttpMethod.DELETE, "/user/**").hasRole("ADMIN");
-
+                    auth.requestMatchers(HttpMethod.PUT, "/v1/**").hasRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.DELETE, "/v1/**").hasRole("ADMIN");
+                    auth.requestMatchers(HttpMethod.POST, "/v1/**").hasRole("ADMIN");
 
                     auth.anyRequest().authenticated();
                 })
